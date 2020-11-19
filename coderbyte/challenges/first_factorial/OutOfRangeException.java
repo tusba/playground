133bcbfdb61e20024a4b0d7875c06f2afb29dd01
tusba <11007086+tusba@users.noreply.github.com>
2020-11-19 12:22:05 +0600

    Add serial version UID to custom exception class according to linter

--- coderbyte/challenges/first_factorial/OutOfRangeException.java
+++ coderbyte/challenges/first_factorial/OutOfRangeException.java
@@ -2,6 +2,8 @@ package coderbyte.challenges.first_factorial;
 
 public class OutOfRangeException extends Exception {
 
+    private static final long serialVersionUID = -4636475791088317303L;
+
     @Override
     public String getMessage() {
         return "The value is out of range";
