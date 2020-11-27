b7695cb8e64b4f7eeb6e50ad66fb5e53169ff3bd
tusba <11007086+tusba@users.noreply.github.com>
2020-11-27 12:04:24 +0600

    Add test for empty results

--- coderbyte/challenges/longest_word/TestChallenge.java
+++ coderbyte/challenges/longest_word/TestChallenge.java
@@ -3,6 +3,9 @@ package coderbyte.challenges.longest_word;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.commons.annotation.Testable;
 
 import util.annotation.ChallengeTest;
@@ -14,10 +17,19 @@ public class TestChallenge implements ITest {
 
     private static final IChallenge challenge = new Challenge();
 
+    @DisplayName("Test sentences containing no words")
+    @ParameterizedTest
+    @Tag(ChallengeTest.TAG)
+    @ValueSource(strings = { "", "!@#", " % - () ." })
+    void noWords(String sentence) {
+        assertEquals("", challenge.longestWord(sentence));
+    }
+
     @ChallengeTest
     @DisplayName("Test longest word")
     @Override
     public void perform() {
+        assertEquals("", challenge.longestWord(null));
         assertEquals("time", challenge.longestWord("fun&!! time"));
         assertEquals("love", challenge.longestWord("I love dogs"));
     }
