3777b07e1bcf4f317734383bfb1e69cee6a92bcd
tusba <11007086+tusba@users.noreply.github.com>
2020-12-13 19:53:02 +0600

    Add "Find Intersection" challenge implementation

--- coderbyte/challenges/find_intersection/IChallenge.java
+++ coderbyte/challenges/find_intersection/IChallenge.java
@@ -6,6 +6,4 @@ public interface IChallenge {
 
     String findIntersection(String[] lists);
 
-    int[] findIntersection(int[] first, int[] second);
-
 }
--- coderbyte/challenges/find_intersection/TestChallenge.java
+++ coderbyte/challenges/find_intersection/TestChallenge.java
@@ -20,6 +20,10 @@ public class TestChallenge implements ITest {
     public void perform() {
         assertEquals("1,4,13", challenge.findIntersection(new String[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" }));
         assertEquals("1,9,10", challenge.findIntersection(new String[] { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" }));
+        assertEquals("6,11,16", challenge.findIntersection(new String[] { "5, 6, 9, 11, 12, 16", "4, 6, 7, 11, 16" }));
+        assertEquals("1,9,10", challenge.findIntersection(new String[] { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" }));
+        assertEquals("11,12", challenge.findIntersection(new String[] { "11, 12, 14, 16, 20", "11, 12, 13, 18, 21" }));
+        assertEquals("21,25", challenge.findIntersection(new String[] { "21, 22, 23, 25, 27, 28", "21, 24, 25, 29" }));
         assertEquals("false", challenge.findIntersection(new String[] { "2, 22", "12, 32" }));
     }
 
